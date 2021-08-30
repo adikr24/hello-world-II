b6dea13397a81337f48f1f0f441469ffb72cab73
Aditya Kumar <adityakumar150794@gmail.com>
2021-08-29 22:43:28 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        //numbers[0] = 1;
+        //numbers[1] = 2;
+        //new j
+        System.out.println("Hello World !");
+    }
+}
